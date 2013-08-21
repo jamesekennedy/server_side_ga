faa232c9395fe8deb2998b5702341d1ff142050c
James Kennedy <james@jameskennedy.ie>
2013-08-21 16:08:50 +0200

    Moved to Ruby 1.8.7 syntax - why not if it makes it more available?

--- lib/server_side_ga.rb
+++ lib/server_side_ga.rb
@@ -13,14 +13,14 @@ module ServerSideGa
    def self.event(tracking_code, client_id, category, action, label = nil, value = nil )
 
     params = {
-      v: @@version,
-      tid: tracking_code,
-      cid: client_id,
-      t: "event",
-      ec: category,
-      ea: action,
-      el: label,
-      ev: value
+      :v => @@version,
+      :tid => tracking_code,
+      :cid => client_id,
+      :t => "event",
+      :ec => category,
+      :ea => action,
+      :el => label,
+      :ev => value
     }
 
     self.hit( params )
@@ -33,17 +33,17 @@ module ServerSideGa
     # report the time differene in miliseconds between event creation time and now
     queue_time = (Time.now - time) * 1000
     params = {
-      v: @@version,
-      tid: tracking_code,
-      cid: client_id,
-      t: "transaction",
-      qt: queue_time.to_s,
-      ti: id,
-      ta: name,
-      tr: revenue,
-      ts: shipping,
-      tt: tax,
-      cu: currency
+      :v => @@version,
+      :tid => tracking_code,
+      :cid => client_id,
+      :t => "transaction",
+      :qt => queue_time.to_s,
+      :ti => id,
+      :ta => name,
+      :tr => revenue,
+      :ts => shipping,
+      :tt => tax,
+      :cu => currency
 
 
     }
